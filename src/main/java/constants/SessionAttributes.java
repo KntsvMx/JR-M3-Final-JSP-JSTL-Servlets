a88545ba49454e439b79c6ecb65b4f1f65fade93
Maksym Kuntsevych <kuntsevychmaksymwork@gmail.com>
2025-02-18 20:01:19 +0200

    feat: update session management to include player attribute and improve inactivity handling
    Took 13 seconds

--- src/main/java/constants/SessionAttributes.java
+++ src/main/java/constants/SessionAttributes.java
@@ -1,8 +1,8 @@
 package constants;
 
 public class SessionAttributes {
-    public static final String USER_NAME = "userName";
-    public static final String GAME_ID = "gameId";
-    public static final String USER_IP = "userIp";
+    public static final String USER_NAME = "username";
     public static final String MAX_INACTIVITY = "maxInactivity";
+    public static final int MAX_INACTIVITY_PERIOD = 30 * 60; // 30 minutes
+    public static final String PLAYER_ATTRIBUTE = "player";
 }
--- src/main/java/util/SessionUtil.java
+++ src/main/java/util/SessionUtil.java
@@ -1,36 +1,17 @@
 package util;
 
 import jakarta.servlet.http.HttpSession;
+import model.Player;
 
-import static constants.SessionAttributes.*;
+import static constants.SessionAttributes.MAX_INACTIVITY;
+import static constants.SessionAttributes.PLAYER_ATTRIBUTE;
 
 public class SessionUtil {
-    public static void setUserNameToSession(HttpSession session, String userName) {
-        session.setAttribute(USER_NAME, userName);
-    }
-
-    public static String getUserNameFromSession(HttpSession session) {
-        return (String) session.getAttribute(USER_NAME);
-    }
-
-    public static void setGameIdToSession(HttpSession session, int gameId) {
-        session.setAttribute(GAME_ID, gameId);
-    }
-
-    public static int getGameIdFromSession(HttpSession session) {
-        return (int) session.getAttribute(GAME_ID);
-    }
-
-
-    public static void setUserIpToSession(HttpSession session, String userIp) {
-        session.setAttribute(USER_IP, userIp);
-    }
-
-    public static String getUserIpFromSession(HttpSession session) {
-        return (String) session.getAttribute(USER_IP);
-    }
-
     public static void setMaxInactivityToSession(HttpSession session, int maxInactivity) {
         session.setAttribute(MAX_INACTIVITY, maxInactivity);
+    }
+
+    public static void setPlayerToSession(HttpSession session, Player player) {
+        session.setAttribute(PLAYER_ATTRIBUTE, player);
     }
 }
