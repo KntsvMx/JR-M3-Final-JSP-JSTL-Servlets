c804c780802fec6e514a6aca63a2d664952cc933
Maksym Kuntsevych <kuntsevychmaksymwork@gmail.com>
2025-02-18 19:11:02 +0200

    feat: refactor Game class to utilize QuestionUtil for question initialization
    Took 30 seconds

--- src/main/java/model/Game.java
+++ src/main/java/model/Game.java
@@ -1,5 +1,7 @@
 package model;
 
+import util.QuestionUtil;
+
 import java.util.ArrayList;
 
 public class Game {
@@ -8,24 +10,9 @@ public class Game {
     private int currentQuestionIndex;
 
     public Game(ArrayList<Question> questions, Player player) {
-        this.questions = initQuestions();
+        this.questions = QuestionUtil.initQuestions();
         this.player = player;
         this.currentQuestionIndex = 0;
-    }
-
-    private ArrayList<Question> initQuestions() {
-        ArrayList<Question> questions = new ArrayList<>();
-        questions.add(new Question("The Sun is planet", false));
-        questions.add(new Question("The Earth is planet", true));
-        questions.add(new Question("Venus is the hottest planet in the Solar System", true));
-        questions.add(new Question("Astronaut experience zero gravity in space", true));
-        questions.add(new Question("The Moon is a star", false));
-        questions.add(new Question("The Earth is flat", false));
-        questions.add(new Question("The Earth is the third planet from the Sun", true));
-        questions.add(new Question("The Earth is the only planet that has life", true));
-        questions.add(new Question("The Earth is the largest planet in the Solar System", false));
-        questions.add(new Question("Mars has liquid water on its surface", false));
-        return questions;
     }
 
 
