7e957bb3d3f52c5e02d9d93ea036f03345f8a91a
Maksym Kuntsevych <kuntsevychmaksymwork@gmail.com>
2025-02-18 19:59:22 +0200

    feat: add GameConstants class for default game settings
    Took 40 minutes

---
+++ src/main/java/constants/GameConstants.java
@@ -0,0 +1,6 @@
+package constants;
+
+public class GameConstants {
+    public static final String DEFAULT_FIRST_QUESTION = "The Sun is planet";
+    public static final Integer DEFAULT_SCORE = 0;
+}
