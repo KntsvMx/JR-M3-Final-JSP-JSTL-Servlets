e0b868028efa759a16c3acba2778423579229efd
Maksym Kuntsevych <kuntsevychmaksymwork@gmail.com>
2025-02-18 19:09:19 +0200

    feat: add SessionAttributes class with session-related constants
    Took 18 seconds

---
+++ src/main/java/constants/SessionAttributes.java
@@ -0,0 +1,8 @@
+package constants;
+
+public class SessionAttributes {
+    public static final String USER_NAME = "userName";
+    public static final String GAME_ID = "gameId";
+    public static final String USER_IP = "userIp";
+    public static final String MAX_INACTIVITY = "maxInactivity";
+}
