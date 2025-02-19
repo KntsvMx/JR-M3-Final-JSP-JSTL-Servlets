4b99b95995ca59023781357eccaf6afcdff38f6e
Maksym Kuntsevych <kuntsevychmaksymwork@gmail.com>
2025-02-19 11:04:49 +0200

    feat: implement GameServlet to manage game sessions and forward to game.jsp
    Took 15 seconds

--- src/main/java/controller/GameServlet.java
+++ src/main/java/controller/GameServlet.java
@@ -1,4 +1,33 @@
 package controller;
 
-public class GameServlet {
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import model.Game;
+import model.Player;
+
+import java.io.IOException;
+
+import static constants.SessionAttributes.GAME_ATTRIBUTE;
+import static constants.SessionAttributes.PLAYER_ATTRIBUTE;
+
+@WebServlet(name = "GameServlet", urlPatterns = {"/game"})
+public class GameServlet extends HttpServlet {
+    private Player player;
+    private Game game;
+
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        player = (Player) req.getSession().getAttribute(PLAYER_ATTRIBUTE);
+        game = new Game(player);
+        req.getSession().setAttribute(GAME_ATTRIBUTE, game);
+        req.getRequestDispatcher("/game.jsp").forward(req, resp);
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+
+    }
 }
