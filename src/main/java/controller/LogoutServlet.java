93b55f1df9adafc8b61f4527e3aa760a086dc2b6
Maksym Kuntsevych <kuntsevychmaksymwork@gmail.com>
2025-03-13 11:29:30 +0200

    refactor: remove unused import statement in LogoutServlet
    Took 1 minute

--- src/main/java/controller/LogoutServlet.java
+++ src/main/java/controller/LogoutServlet.java
@@ -5,7 +5,6 @@ import jakarta.servlet.http.HttpServlet;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import jakarta.servlet.http.HttpSession;
-import jdk.jpackage.internal.Log;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
